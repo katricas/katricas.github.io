e8de7c4eca8bd5afd54411aab1ca188741bb550a
katricas <54758704+katricas@users.noreply.github.com>
2023-03-24 12:48:46 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const toggleSkillsButton = document.getElementById('toggle-skills');
+const skillsList = document.getElementById('skills-list');
+
+toggleSkillsButton.addEventListener('click', () => {
+  if (skillsList.style.display === 'none') {
+    skillsList.style.display = 'block';
+    toggleSkillsButton.textContent = 'Hide Skills';
+  } else {
+    skillsList.style.display = 'none';
+    toggleSkillsButton.textContent = 'Show Skills';
+  }
+});
+
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({behavior: "smooth"});
+}
